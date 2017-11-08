78592de1a9e4d646a4345c7bedfd11742b5c85bf
qres <qres@users.noreply.github.com>
2017-11-08 14:32:42 +0100

    make top and right boundary at half cell
    no unnecessary grid points at boundaries

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -58,14 +58,13 @@ void Geometry::Update_U(Grid *u) const {
     it.SetBoundary(Boundary::Right);
     for(it.First(); it.Valid(); it.Next()) {
         // no slip, staggered grid => set left (= boundary) point to zero
-        u->Cell(it) = 0.0; // TODO what happens with this point?
-        u->Cell(it.Left()) = 0.0;
+        u->Cell(it) = -u->Cell(it.Left());
     }
 
     it.SetBoundary(Boundary::Top);
     for(it.First(); it.Valid(); it.Next()) {
         // velocity given
-        u->Cell(it) = 2* this->_velocity[0] - u->Cell(it.Down());
+        u->Cell(it) = this->_velocity[0];
     }
 }
 
@@ -87,14 +86,13 @@ void Geometry::Update_V(Grid *v) const {
     it.SetBoundary(Boundary::Right);
     for(it.First(); it.Valid(); it.Next()) {
         // no slip => define boundary s.t. the interpolated value is zero
-        v->Cell(it) = - v->Cell(it.Left());
+        v->Cell(it) = 0.0;
     }
 
     it.SetBoundary(Boundary::Top);
     for(it.First(); it.Valid(); it.Next()) {
         // velocity given
-        v->Cell(it) = this->_velocity[1]; // TODO what happens with this point?
-        v->Cell(it.Down()) = this->_velocity[1];
+        v->Cell(it) = 2.0 * this->_velocity[1] - v->Cell(it.Down());
     }
 }
 
@@ -123,6 +121,5 @@ void Geometry::Update_P(Grid *p) const {
     for(it.First(); it.Valid(); it.Next()) {
         // after choosong F_0,j := u_0,j we get homogeneous Neumann boundary conditions
         p->Cell(it) = p->Cell(it.Down());
-
     }
 }
