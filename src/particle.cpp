b7f22b5fb6b230e5101e758a4aa9b818c8cefd25
qres <qres@users.noreply.github.com>
2017-12-09 16:58:08 +0100

    merge Blatt3

---
+++ src/particle.cpp
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2015   Malte Brunn
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "particle.hpp"
+#include "geometry.hpp"
+#include "grid.hpp"
+#include <stdio.h>
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+void ParticleLine::SaveVTK (const index_t& rank, const index_t& nump, const char* basename, const index_t& idx) const {
+  char fname[1000];
+  const char *filebase = basename;
+  for (int i = 0; basename[i] != 0; ++i) {
+    if (basename[i] == '/') filebase = &basename[i+1];
+    if (basename[i] == '\\') filebase = &basename[i+1];
+  } 
+  if (rank == 0) {
+    sprintf(fname, "%s_%05i.pvtp", basename, idx);
+    FILE *handle = fopen(fname, "w");
+    fprintf(handle, "<?xml version=\"1.0\"?>\n"
+                    "<VTKFile type=\"PPolyData\" version=\"0.1\" "
+                    "byte_order=\"LittleEndian\">\n\t"
+                    "<PPolyData GhostLevel=\"0\">\n\t\t<PPoints>\n\t\t\t"
+                    "<PDataArray type=\"Float32\" Name=\"Position\" "
+                    "NumberOfComponents=\"3\" format=\"ascii\"/>\n"
+                    "\t\t</PPoints>\n\t\t"
+                    "<PPointData Scalars=\"mpirank\">\n\t\t\t"
+                    "<PDataArray type=\"Int32\" Name=\"mpirank\" "
+                    "format=\"ascii\"/>\n\t\t\t"
+                    "</PPointData>\n");
+    for (int p = 0; p < nump; ++p)
+      fprintf(handle, "\t\t<Piece Source=\"%s_%05i_%04i.vtp\"/>\n", filebase, idx, rank);
+    fprintf(handle, "\t</PPolyData>\n</VTKFile>\n");
+    fclose(handle);
+  }
+  sprintf(fname, "%s_%05i_%04i.vtp", basename, idx, rank);
+  FILE *handle = fopen(fname, "w");
+  fprintf(handle, "<?xml version=\"1.0\"?>\n"
+                  "<VTKFile type=\"PolyData\" version=\"0.1\" "
+                  "byte_order=\"LittleEndian\">\n\t");
+  fprintf(handle, "<PolyData>\n\t\t<Piece NumberOfPoints=\"%i\" NumberOfVerts=\"0\" ", _part.size());
+  fprintf(handle, "NumberOfLines=\"0\" NumberOfStrips=\"0\" "
+                  "NumberOfPolys=\"0\">\n\t\t\t<Points>\n\t\t\t\t"
+                  "<DataArray type=\"Float32\" Name=\"Position\" "
+                  "NumberOfComponents=\"3\" format=\"ascii\">\n");
+  for (int p = 0; p < _part.size(); ++p) {
+    fprintf(handle, "\t\t\t\t\t%le %le %le\n", _part[p].Pos()[0], _part[p].Pos()[1], 0.0);
+  }
+  fprintf(handle, "\t\t\t\t</DataArray>\n\t\t\t</Points>\n\t\t\t"
+                  "<PointData Scalars=\"mpirank\">\n\t\t\t\t"
+                  "<DataArray type=\"Int32\" Name=\"mpirank\" "
+                  "format=\"ascii\">\n\t\t\t\t\t");
+  for (int p = 0; p < _part.size(); ++p) {
+    fprintf(handle, "%i ", rank);
+  }
+  fprintf(handle, "\n\t\t\t\t</DataArray>\n\t\t\t</PointData>\n"
+                  "\t\t</Piece>\n\t</PolyData>\n</VTKFile>\n");
+  fclose(handle);
+}
+//------------------------------------------------------------------------------
+
---
+++ src/particle.hpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2015   Malte Brunn
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "typedef.hpp"
+#include <vector>
+//------------------------------------------------------------------------------
+#ifndef __PARTICLE_HPP
+#define __PARTICLE_HPP
+//------------------------------------------------------------------------------
+class Particle {
+public:
+  Particle (const multi_real_t& pos);
+  ~Particle ();
+  void TimeStep(const real_t& dt, const Grid* u, const Grid* v);
+  const multi_real_t& Pos () const;
+private:
+  multi_real_t _pos;
+};
+//------------------------------------------------------------------------------
+class ParticleLine {
+public:
+  virtual void TimeStep(const real_t& dt, const Grid* u, const Grid* v) = 0;
+  virtual void SaveVTK(const index_t& rank, const index_t& nump, const char* basename, const index_t& idx) const;
+protected:
+  std::vector<Particle> _part;
+};
+//------------------------------------------------------------------------------
+class PathLine : public ParticleLine {
+public:
+  PathLine (const multi_real_t& pos);
+  ~PathLine ();
+  void TimeStep(const real_t& dt, const Grid* u, const Grid* v);
+};
+//------------------------------------------------------------------------------
+class StreakLine : public ParticleLine {
+public:
+  StreakLine (const multi_real_t& pos);
+  ~StreakLine ();
+  void TimeStep(const real_t& dt, const Grid* u, const Grid* v);
+private:
+  const multi_real_t& _org;
+};
+//------------------------------------------------------------------------------
+#endif  // __PARTICLE_HPP
